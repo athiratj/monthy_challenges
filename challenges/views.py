3f1abdb9b83e843d394f396b1356522047f74f58
athira <athiratjthanaly83@gmail.com>
2021-05-13 16:45:06 +0530

    set different path for numbers

--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+def monthly_challenge_number(request, month):
+    return HttpResponse(month)
+    
 def monthly_challenge(request, month):
     challenge_text=None
     if month=='jan':

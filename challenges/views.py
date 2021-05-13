e6f74d68741fd1cc2609784559099b9749538a88
athira <athiratjthanaly83@gmail.com>
2021-05-13 23:40:22 +0530

    add redirect url from numbers to months

--- challenges/views.py
+++ challenges/views.py
@@ -1,17 +1,25 @@
 from django.http.response import HttpResponseNotFound
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 # Create your views here.
 
-def monthly_challenge_number(request, month):
-    return HttpResponse(month)
-    
 challenge ={
     'jan':'Only vegetables in food!',
     'feb':'Exercise daily!',
     'mar':'Study aptitude for a month!',
     'apr':'Practice coding'
-}    
+}  
+
+
+def monthly_challenge_number(request, month):
+    months=list(challenge.keys())
+    
+    if month>len(months):
+        return HttpResponseNotFound('No such a month found!')
+    redirect_month=months[month-1]
+    return HttpResponseRedirect('/challenges/'+redirect_month)
+    
+  
 def monthly_challenge(request, month):
     try:
         challenge_text=challenge[month] 
